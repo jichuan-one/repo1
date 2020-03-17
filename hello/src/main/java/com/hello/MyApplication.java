e203cebf0ac2b4f020e017ce20ac03c1a9dc57be
jichuan <1648035697@qq.com>
2020-03-17 18:29:55 +0800

    将hello项目添加到版本库，刚好像操作错误了

---
+++ hello/src/main/java/com/hello/MyApplication.java
@@ -0,0 +1,12 @@
+package com.hello;
+
+import org.springframework.boot .SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args){
+        //Spring应用启动起来
+        SpringApplication.run(MyApplication.class,args);
+    }
+}
